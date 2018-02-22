b2e2eab54d184e6bb672b4a5305e198fc71034c7
Roy Hooper <rhooper@toybox.ca>
2018-02-22 12:07:27 -0500

    fix lint

--- djangocms_conditional/models.py
+++ djangocms_conditional/models.py
@@ -3,7 +3,6 @@ from cms.models import CMSPlugin
 from django.contrib.auth.models import Group
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
-from django.utils.timezone import now
 from django.utils.translation import ugettext_lazy as _
 
 
