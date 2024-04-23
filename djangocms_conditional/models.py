81535f25d382e7aa24202041317d8694c459e2c3
Adrien Delhorme <ad@kapt.mobi>
2024-04-23 17:19:58 +0200

    add french translation

--- djangocms_conditional/models.py
+++ djangocms_conditional/models.py
@@ -13,9 +13,10 @@ MODE_ANONYMOUS = 'anonymous'
 
 class ConditionalPluginModel(CMSPlugin):
 
-    permitted_group = models.ForeignKey(Group, null=True, blank=True, on_delete=models.SET_NULL)
+    permitted_group = models.ForeignKey(Group, null=True, blank=True, on_delete=models.SET_NULL, verbose_name=_("Permitted group"))
     mode = models.CharField(max_length=40,
                             default='in_group',
+                            verbose_name=_("Mode"),
                             help_text=_("Conditional access type"),
                             choices=((MODE_IN_GROUP, _('Users in group')),
                                      (MODE_NOT_IN_GROUP, _('Users not in group')),
@@ -25,9 +26,9 @@ class ConditionalPluginModel(CMSPlugin):
 
     def __str__(self):
         if self.permitted_group is not None:
-            return _('%s: %s') % (
-                self.get_mode_display(),
-                self.permitted_group.name,
+            return _('{mode}: {group}').format(
+                mode=self.get_mode_display(),
+                group=self.permitted_group.name,
             )
         else:
             return self.get_mode_display()
